0ccb5237b62579e87b7b41f4e0b053c0e07b49e3
curtcurt69 <cecrouse96@yahoo.com>
2023-12-20 05:03:07 -0500

    added search functionality to the feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -18,12 +18,9 @@ const PromptCardList = ({ data, handleTagClick }) => {
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
   const [posts, setPosts] = useState([]);
-
-  const handleSearchChange= (e) => {
-
-  };
-
   
   useEffect(() => {
     const fetchPosts = async () => {
@@ -37,6 +34,32 @@ const Feed = () => {
     fetchPosts();
   }, []);
 
+  const filterPrompts = (search_text) => {
+    const regex = new RegExp(search_text, "i");
+    
+    return posts.filter(
+      (item) => regex.test(item.creator.username) || regex.test(item.tag) || regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange= (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+    
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  }
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -50,10 +73,18 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList 
+      {searchText ? (
+        <PromptCardList 
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList 
         data={posts}
-        handleTagClick={() => {}}
+        handleTagClick={handleTagClick}
       />
+      )}
+      
 
     </section>
   )
